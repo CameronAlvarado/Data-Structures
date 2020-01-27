2bebd06034466fe291213243cc6458e53fe622bf
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2020-01-27 13:28:30 -0800

    added some code from lecture

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,21 @@
-import sys
-sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 
 class Queue:
     def __init__(self):
-        self.size = 0
+        # self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # Because stacks and queues deal with order of values.
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_head(value)
+        return True
 
     def dequeue(self):
-        pass
+        if self.len() > 0:
+            return self.storage.remove_from_tail()
+        return None
 
     def len(self):
-        pass
+        return len(self.storage)
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,12 +1,13 @@
-import sys
-sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('./doubly_linked_list')
+
 
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList
 
     def push(self, value):
         pass
@@ -15,4 +16,4 @@ class Stack:
         pass
 
     def len(self):
-        pass
+        return len(self.storage)
