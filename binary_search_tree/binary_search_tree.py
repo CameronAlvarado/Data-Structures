85edfddb1425690cc4518680cd261523c328b60e
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2020-01-29 21:09:35 -0800

    done with BST

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -3,14 +3,14 @@ from dll_queue import Queue
 import sys
 
 
-class BinarySearchTree:
+class BinarySearchTree:  # a node of the tree
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
     # Insert the given value into the tree
-    def insert(self, value):  # MVP
+    def insert(self, value):
         if value < self.value:
             if self.left is None:
                 self.left = BinarySearchTree(value)
@@ -26,35 +26,40 @@ class BinarySearchTree:
 
     # Return True if the tree contains the value
     # False if it does not
-    def contains(self, target):  # MVP
-        if target is not self.value:
-            if target is not self.left:
-                new_value = self.left
-                if self.contains(new_value) is False:
-                    return False
-            if target is not self.right:
-                new_value = self.right
-                if self.contains(new_value) is False:
-                    return False
-            else:
-                return True
-        else:
+    def contains(self, target):
+        if target is self.value:
             return True
+        elif target > self.value:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+        elif target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
 
-        # Return the maximum value found in the tree
-
-    def get_max(self):  # MVP
-        pass
+    # Return the maximum value found in the tree
+    def get_max(self):  # visit every right node
+        if self.right is None:
+            return self.value
+        else:
+            return self.right.get_max()
 
         # Call the function `cb` on the value of each node
         # You may use a recursive or iterative approach
-    def for_each(self, cb):  # MVP
-        pass
+    def for_each(self, cb):
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
 
         # DAY 2 Project -----------------------
 
-        # Print all the values in order from low to high
-        # Hint:  Use a recursive, depth first traversal
+    # Print all the values in order from low to high
+    # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
         pass
 
